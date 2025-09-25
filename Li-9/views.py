d48ab3d80a247c86f242b8fcc0422ab02b2f1528
B1EL124 <161520000+B1EL124@users.noreply.github.com>
2025-09-25 14:19:52 +0000

    CHUPETINHA

--- Li-9/views.py
+++ Li-9/views.py
@@ -5,6 +5,8 @@ from models.servico import Servico, ServicoDAO
 class View:
     def cliente_listar():
         return ClienteDAO.listar()
+    def cliente_listar_id(id):
+        return ClienteDAO.listar_id(id)
     def cliente_inserir(nome, email, fone):
         cliente = Cliente(0, nome, email, fone)
         ClienteDAO.inserir(cliente)
@@ -17,6 +19,8 @@ class View:
 
     def servico_listar():
         return ServicoDAO.listar()
+    def servico_listar_id(id):
+        return ServicoDAO.listar_id(id)
     def servico_inserir(descricao, valor):
         servico = Servico(0, descricao, valor)
         ServicoDAO.inserir(servico)
